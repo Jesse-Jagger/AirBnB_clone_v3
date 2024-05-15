42ebd51d32721c911eed73ab3229a389b5ea86b6
jesse-jagger <jessejackson.055@gmail.com>
2024-05-15 16:22:37 -0700

    updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 flask instance"""
 
 from os import getenv
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -13,7 +13,18 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
+@app.teardown_appcontext
+def teardown_engine(exception):
+    """closes the storage"""
+    storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    """returns a 404 page"""
+    return jsonify({"error": "Not found"}), 404
+
 if __name__ == '__main__':
     HOST = getenv('HBNB_API_HOST', '0.0.0.0')
     PORT = int(getenv('HBNB_API_PORT', 5000))
-    app.run(host=HOST, port=PORT, threaded=True,)
+    app.run(host=HOST, port=PORT, threaded=True, debug=True)
